7d20cfb8730f5fc6407f7eb9fd9c54ef405830fd
amartyamalviya-ai <amartyamalviya@gmail.com>
2026-08-09 13:25:02 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int i=0;
+        int n=nums.size();
+        while(i<n){
+            nums.push_back(nums[i]);
+            i++;
+        }
+        return nums;
+    }
+};
